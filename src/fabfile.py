9b935edf4128d4dc1fb411b3425d0418de65c55c
Rohan Roy <rohanroy@Rohans-MacBook-Pro.local>
2016-08-01 02:40:54 +0530

    celery and rabbitmq integration complete

---
+++ src/fabfile.py
@@ -0,0 +1,15 @@
+from fabric.api import local
+
+def push():
+	local('git status')
+	print "you can create a separate branch for this changes!"
+	inp = raw_input("Enter Y to continue:")
+	print "inp:",inp
+	if inp == "Y" or inp == 'y':
+		local('git add .')
+		commit_message = raw_input("Enter commit message:")
+		local('git commit -m "%s"'%commit_message)
+		branch_name = local('git name-rev --name-only HEAD')
+		print "Current branch name: %s"%branch_name,
+		local('git push origin %s'%branch_name)
+
