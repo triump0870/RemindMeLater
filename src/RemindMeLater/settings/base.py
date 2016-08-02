d2130002e82aab84e5b588fba165caeffb2a8e9a
Rohan Roy <rohanroy@Rohans-MacBook-Pro.local>
2016-08-02 18:02:32 +0530

    updated

--- src/RemindMeLater/settings/base.py
+++ src/RemindMeLater/settings/base.py
@@ -125,10 +125,10 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.DjangoModelPermissions',
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
     ]
 }
-
 # Crispy Form Theme - Bootstrap 3
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
--- src/Reminder/models.py
+++ src/Reminder/models.py
@@ -4,12 +4,13 @@ from django.core.validators import RegexValidator
 # Create your models here.
 class Reminder(models.Model):
 	message = models.CharField(max_length=500)
-	time = models.DateTimeField(editable=True)
+	date = models.DateField(editable=True)
+	time = models.TimeField(editable=True)
 	created = models.DateTimeField(auto_now_add=True)
 	phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
 	phone_number = models.CharField(max_length=15,validators=[phone_regex], blank=True, null=True)
 	email = models.EmailField(max_length=100, blank=True, null=True)
-	
+	completed = models.BooleanField(default=False)
 	def __unicode__(self):
 		return 'Reminder #{0}'.format(self.pk)
 
