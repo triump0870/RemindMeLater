19670fc17c95816feb531b1a2d10b3e7c35e8fd3
Rohan Roy <rohanroy@Rohans-MacBook-Pro.local>
2016-08-02 16:26:26 +0530

    updated

--- src/RemindMeLater/urls.py
+++ src/RemindMeLater/urls.py
@@ -17,7 +17,8 @@ urlpatterns = [
 ]
 
 # User-uploaded files like profile pics need to be served in development
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += [
+		url(r'^media/(?P<path>.*)$', 'django.views.static.serve',{'document_root':settings.MEDIA_ROOT}),
         url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
     ]
