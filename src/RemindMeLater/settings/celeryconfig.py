bf6e4aeb073e338e5eeb310062f5fd6e1536d91c
Rohan Roy <rohanroy@Rohans-MacBook-Pro.local>
2016-08-04 22:27:25 +0530

    Updated

--- src/RemindMeLater/settings/celeryconfig.py
+++ src/RemindMeLater/settings/celeryconfig.py
@@ -1,3 +1,4 @@
+BROKER_URL = 'amqp://'
 CELERY_RESULT_BACKEND = 'rpc://'
 BROKER_POOL_LIMIT = 3
 
@@ -7,15 +8,15 @@ CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TIMEZONE = 'Asia/Kolkata'
 CELERY_ENABLE_UTC = True
 CELERY_SEND_TASK_ERROR_EMAILS = True
-SERVER_EMAIL = 'abc@example.com'
+SERVER_EMAIL = 'rohan@rohanroy.com'
 ADMINS = [
-        ('abc', 'abc@example.com')
+        ('Rohan', 'rohan@rohanroy.com')
 ]
 EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
-DEFAULT_FROM_EMAIL = 'abc@example.com'
+DEFAULT_FROM_EMAIL = 'rohan@rohanroy.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 465
 EMAIL_TIMEOUT = 10
 EMAIL_HOST = 'email-smtp.us-east-1.amazonaws.com'
-EMAIL_HOST_USER = 'abcjdjdlasskjjdklsaj'
-EMAIL_HOST_PASSWORD = 'djkashdklahsjdhasljkdhjksahdjkashdjakhdak'
+EMAIL_HOST_USER = 'AKIAICOLHTWXCT6E7UKQ'
+EMAIL_HOST_PASSWORD = 'AsFxRevnYm6eEXtLKvrnjBCzsCVUtS1UBxGGaygXB3B+'
