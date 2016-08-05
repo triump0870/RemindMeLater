9cac927c454f7b5cc39de489c70816db771251ee
Rohan Roy <rohanroy@Rohans-MacBook-Pro.local>
2016-08-05 21:09:22 +0530

    Updated

--- src/RemindMeLater/settings/production.py
+++ src/RemindMeLater/settings/production.py
@@ -45,12 +45,6 @@ DATABASES = {
 }
 
 
-RAVEN_CONFIG = {
-    'dsn': 'https://67f46cd6b820412e8f0d4d94cc832486:36fba09345934d469d45b0b298e7e87b@app.getsentry.com/89988',
-    # If you are using git, you can also automatically configure the
-    # release based on the git info.
-}   
-
 # production database settings
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
