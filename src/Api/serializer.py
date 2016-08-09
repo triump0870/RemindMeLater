295d909f063d9b155cdb7b2273ecae63490377d9
Rohan Roy <rohanroy@Rohans-MacBook-Pro.local>
2016-08-09 17:21:44 +0530

    [Fixed] Phone number validation

--- src/Api/serializer.py
+++ src/Api/serializer.py
@@ -19,11 +19,11 @@ class ReminderSerializer(serializers.ModelSerializer):
 
 	def validate(self, data):
 		phone_number,email = data['phone_number'],data['email']
-		regex =re.compile('^\+1?\d{12,15}$')
-		phone = regex.match(phone_number)
-		
-		if phone is None:
-			raise serializers.ValidationError({"phone_number":"Phone number must be entered in the format: '+919876543210'. Up to 15 digits allowed."})
+		if phone_number is not None:
+			regex =re.compile('^\+1?\d{12,15}$')
+			phone = regex.match(phone_number)
+			if phone is None:
+				raise serializers.ValidationError({"phone_number":"Phone number must be entered in the format: '+919876543210'. Up to 15 digits allowed."})
 		
 		if not email and not phone_number:
 			raise serializers.ValidationError({"phone_number":"Phone number was not provided","email":"Email was not provided"})
